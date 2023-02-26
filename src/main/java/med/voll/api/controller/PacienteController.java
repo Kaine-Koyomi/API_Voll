d7bd2ac5b6b76c86c100f5c92f79257767309115
Kaine-Koyomi <arararagisp@gmail.com>
2023-02-26 19:58:33 -0300

    Get method for Medico and Paciente created

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,7 +1,10 @@
 package med.voll.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosListagemPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 
@@ -23,5 +27,10 @@ public class PacienteController {
     @Transactional
     public void registrar(@RequestBody @Valid DadosCadastroPaciente json){
         repository.save(new Paciente(json));
+    }
+
+    @GetMapping
+    public List<DadosListagemPaciente> listar(){
+        return repository.findAll().stream().map(DadosListagemPaciente::new).toList();
     }
 }
