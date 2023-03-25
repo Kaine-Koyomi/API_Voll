f46eaa0ce9284fbeeb2e53971b9f0f9d846ff759
Kaine-Koyomi <arararagisp@gmail.com>
2023-03-25 16:26:22 -0300

    creating user table

---
+++ src/main/java/med/voll/api/user/User.java
@@ -0,0 +1,21 @@
+package med.voll.api.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name="users")
+@Entity(name= "User")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String name;
+    String password;
+}
